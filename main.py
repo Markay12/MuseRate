33ae718aea9f572e8873b12992fcc0ef61ebabad
cache <ashinhust.brass@gmail.com>
2021-08-13 18:09:46 -0700

    updated with token

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 client.add_cog(musicRate(client))
 
 
-client.run("ODc1ODcyMjU4MDc2MzExNjQy.YRb1mw.bDF-qSUriAGnF7l0fYxdiDvKr1g")
+client.run(os.environ['TOKEN'])
