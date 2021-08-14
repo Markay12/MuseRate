a4aa0c07df6c2e3ebe7938c48abb8d2a6d675264
cache <ashinhust.brass@gmail.com>
2021-08-14 07:07:29 -0700

    change keyword

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from musicRate import musicRate
 import os
 
 
-client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
 
 client.add_cog(musicRate(client))
 
